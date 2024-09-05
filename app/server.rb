7e29452941114ce0593809f924b4f0a033fdbd95
Ray Plain <rayplain@gmail.com>
2024-09-05 14:46:47 -0500

    Adding client and responding to ping

--- app/server.rb
+++ app/server.rb
@@ -10,8 +10,22 @@ class YourRedisServer
     puts("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    # server = TCPServer.new(@port)
-    # client = server.accept
+    server = TCPServer.new(@port)
+    client = server.accept
+
+    loop do
+      request = client.gets
+      if request.start_with?("*1") && client.gets == "$4\r\n" && client.gets.chomp == "PING"
+        client.puts string_to_regular_string "PONG"
+      else
+        client.puts string_to_regular_string "Unknown Command"
+      end
+    end
+  end
+
+  private
+  def string_to_regular_string(string)
+    "+#{string}\r\n"
   end
 end
 
