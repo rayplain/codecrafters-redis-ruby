e83a9f74de6e99f5a1321d46e5f009221345580b
Ray Plain <rayplain@gmail.com>
2024-09-05 17:02:37 -0500

    added naive handling of echo for now

--- app/server.rb
+++ app/server.rb
@@ -20,15 +20,29 @@ class YourRedisServer
     end
   end
 
+
   def handle_client(client)
     while (line = client.gets) do
       client.puts string_to_regular_string "PONG" if line.include? "PING"
+      if line.include? "ECHO"
+        client.gets
+        client.puts(string_to_bulk_string(client.gets)) if line.include? "ECHO"
+      end
     end
   end
 
   private
   def string_to_regular_string(string)
     "+#{string}\r\n"
+  end
+
+  def string_to_bulk_string(string)
+    "$#{string.bytesize}\r\n#{string}\r\n"
+  end
+
+  def parse_command_segments(command)
+    puts "Splitting the command: #{command}"
+    command.split(/\$[0-9]+/).map(&:strip)
   end
 end
 
