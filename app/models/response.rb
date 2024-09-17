3bcdf4f4a12c665b3c1bd844bce94b08d944bcd3
Ray Plain <rayplain@gmail.com>
2024-09-17 17:18:29 -0500

    WIP: Basic build out of response class

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,22 @@
 class Response
-  #TODO add formatted responses here.
+  VALID_COMMANDS = %w(regular bulk error null_bulk).freeze
+  def initialize(command:, type:)
+    @command = command
+    @type = type
+  end
+  def encode
+    case @type
+    when :regular
+      "+#{@command}\r\n"
+    when :bulk
+      "$#{@command.bytesize}\r\n#{@command}\r\n"
+    when :error
+      "-#{@command}\r\n"
+    when :null_bulk
+      "$-1\r\n"
+    else
+      raise ArgumentError, "Invalid response type: #{@type}"
+    end
+  end
+
 end
